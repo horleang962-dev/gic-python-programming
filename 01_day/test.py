27052a3a9f36b3c12449d9ef11847423a22e0ac3
horleang962-dev <horleang962@gmail.com>
2026-05-22 11:59:25 +0700

    First push: Day 1 python basics

---
+++ 01_day/test.py
@@ -0,0 +1,36 @@
+#1Printing (print())
+print("Hello well come to python class")
+print("Hello LeangHor well come to python class\n")
+
+#Manipulate text strings ( + )
+
+print("Hello well come to pytho clas\n" + "Hell LeangHor well come to python class")
+
+#2Input Function (input()): Learning how to prompt the user for data and read their text response directly into the program.
+
+input("Enter a Number:")
+
+#3Nested Functions
+
+length = len(input("Enter your name:"))
+print(length)
+
+Total_age = int(input("How old are you?"))
+print(Total_age + 18)
+
+#5Variables
+
+name = "THON LEANGHOR"
+age = Total_age
+city = "Phnom Penh"
+
+print(name)
+print(age)
+print(city)
+
+#The computer always remembers the latest value!
+ x = 10
+ x = 90
+ print(x) #output 90
+
+#6
